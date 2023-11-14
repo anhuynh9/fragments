64c33d7096cd9b47b31a6a60f3f811cdf519f63e
anhuynh9 <lmai8084@gmail.com>
2023-11-14 06:44:00 +0530

    Assignement 02

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -11,6 +11,27 @@ const router = express.Router();
 // Define our first route, which will be: GET /v1/fragments
 router.get('/fragments', require('./get'));
 
-// Other routes will go here later on...
+// Get fragments by id: GET /v1/fragments/:id
+router.get('/fragments/:id', require('./getById'));
+
+// Get fragments info by id: GET /v1/fragments/:id/info
+router.get('/fragments/:id/info', require('./getById'));
+
+const rawBody = () =>
+  express.raw({
+    inflate: true,
+    limit: '5mb',
+    type: (req) => {
+      // See if we can parse this content type. If we can, `req.body` will be
+      // a Buffer (e.g., `Buffer.isBuffer(req.body) === true`). If not, `req.body`
+      // will be equal to an empty Object `{}` and `Buffer.isBuffer(req.body) === false`
+      const { type } = contentType.parse(req);
+      return Fragment.isSupportedType(type);
+    },
+  });
+
+// Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
+// You can use Buffer.isBuffer(req.body) to test if it was parsed by the raw body parser.
+router.post('/fragments', rawBody(), require('./post'));
 
 module.exports = router;
