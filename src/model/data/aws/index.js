2d199d634a18a84bf1b917e8cd5bff3a33753efb
Anhuynh <lmai8084@gmail.com>
2023-12-05 09:14:07 +0530

    Lab 10

--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -4,7 +4,7 @@ const logger = require('./logger');
 
 // Create two in-memory databases: one for fragment metadata and the other for raw data
 //const data = new MemoryDB();
-const metadata = new MemoryDB();
+// const metadata = new MemoryDB();
 
 const s3Client = require('./s3Client');
 const ddbDocClient = require('./ddbDocClient');
